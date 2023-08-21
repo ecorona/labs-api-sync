11abf1788ffe6fd6e8346d93700738370f793f53
Erik Corona <ecorona@live.com>
2023-08-21 13:11:38 -0600

    inicial

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(3000);
+}
+bootstrap();
